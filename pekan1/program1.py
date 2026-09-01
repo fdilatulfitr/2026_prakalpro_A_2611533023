0dc325f990a1774f609ca2f5e21035b11df5215b
fdilatulfitr <muhammadfaiz050607@gmail.com>
2026-09-01 10:01:23 +0700

    praktikum1 alpro selasa

---
+++ pekan1/program1.py
@@ -0,0 +1,12 @@
+#program pertama
+"""
+komentar
+dua baris atau lebih
+pertemuan 1 praktikum alpro
+"""
+print("Hello world")
+print()
+print("program ini menghasilkan")
+print("empat baris output")
+print("/ \\ // \\\\ /// \\\\\\")
+print("program ini" + "dibuat oleh a/n"+"Fadilatul Fitri 2611533023")
